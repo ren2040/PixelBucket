ee02b37432c04ec04fe1e898cd528b028aeb4734
Renji <renjishaji@outlook.com>
2019-02-10 17:50:32 +0000

    "First commit"

---
+++ DomainContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PixelBucket.Models
+{
+    public class DomainContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+
+        public DomainContext(DbContextOptions<DomainContext> options)
+            : base(options) { }
+
+        public Microsoft.EntityFrameworkCore.DbSet<Item> Items {get; set;}
+    }
+}
---
+++ Migrations/Domain/20190209233308_[Added_Item].cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PixelBucket.Migrations.Domain
+{
+    public partial class Added_Item : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    ItemId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.ItemId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+        }
+    }
+}
